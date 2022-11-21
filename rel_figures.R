b0a90bcbaa27d903f4441ffa4479155fc3d8fd85
sgaiesky <sgaiesky@gmail.com>
2022-11-21 12:49:05 -0800

    addition of cv and asym plots

--- rel_figures.R
+++ rel_figures.R
@@ -17,15 +17,21 @@ long_rel <- dat %>%
   filter(time == "3-mth")
 
 plot_lab <- c("Baseline", "1-week", "3-months")
-facet_lab <- as_labeller(c("peak_result" = "Peak Resultant",
+peak_facet_lab <- as_labeller(c("peak_result" = "Peak Resultant",
                          "peak_vert" = "Peak Vertical",
                          "peak_ap" = "Peak Anterior-Posterior"))
+cv_facet_lab <- as_labeller(c("cv_result" = "Resultant",
+                                "cv_vert" = "Vertical",
+                                "cv_ap" = "Anterior-Posterior"))
+asym_facet_lab <- as_labeller(c("asym_result" = "Resultant",
+                              "asym_vert" = "Vertical",
+                              "asym_ap" = "Anterior-Posterior"))
 text_size <- 10
 text_angle <- 0
 
 #facet_wrap based on variable...?
 
-test_plot <- ggplot(data = filter(dat,
+peak_plot <- ggplot(data = filter(dat,
                      !grepl("cv", variable) & !grepl("asym", variable) &measure == "mean"),
                mapping = aes(x = time, y = value)) +
   geom_errorbar(mapping = aes(ymin = value - sd, ymax = value + sd,
@@ -38,7 +44,7 @@ test_plot <- ggplot(data = filter(dat,
                           colour = speed)) +
   labs(
     x = "",
-    y = "Average Acceleration (g's)",
+    y = "Peak Acceleration (g)",
     colour = "Speed"
   ) +
   guides(group = "none") +
@@ -60,9 +66,87 @@ test_plot <- ggplot(data = filter(dat,
         strip.text = element_text(face = "bold", size = text_size + 2),
         strip.background = element_blank()) +
   facet_wrap(vars(variable),
-             labeller = facet_lab)
+             labeller = peak_facet_lab)
 
-print(test_plot)
+cv_plot <- ggplot(data = filter(dat,
+                                  !grepl("peak", variable) & !grepl("asym", variable) &measure == "mean"),
+                    mapping = aes(x = time, y = value)) +
+  geom_errorbar(mapping = aes(ymin = value - sd, ymax = value + sd,
+                              colour = speed),
+                width = 0.05) +
+  geom_point(mapping = aes(colour = speed),
+             shape = 19,
+             size = 3) +
+  geom_line(mapping = aes(group = speed,
+                          colour = speed)) +
+  labs(
+    x = "",
+    y = "Coefficient of Variation (%)",
+    colour = "Speed"
+  ) +
+  guides(group = "none") +
+  scale_x_discrete(labels = plot_lab,
+                   expand = c(0.15, 0)) +
+  scale_y_continuous(expand = c(0,0),
+                     limits = c(0,15)) +
+  scale_color_manual(labels = c("2.5 m/s", "3.0 m/s", "3.5 m/s"),
+                     values = c("#FC8D59", "#D7301F", "#7F0000")) +
+  theme_classic() +
+  theme(legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.text = element_text(face = "bold", size = text_size),
+        legend.title = element_text(face = "bold", size = text_size),
+        axis.text.x = element_text(colour = "black", face = "bold", 
+                                   size = text_size, angle = text_angle),
+        axis.title.y = element_text(face = "bold", size = text_size),
+        strip.placement = "outside",
+        strip.text = element_text(face = "bold", size = text_size + 2),
+        strip.background = element_blank()) +
+  facet_wrap(vars(variable),
+             labeller = cv_facet_lab)
 
-ggsave("test_plot.png", plot = test_plot)
+asym_plot <- ggplot(data = filter(dat,
+                                !grepl("peak", variable) & !grepl("cv", variable) &measure == "mean"),
+                  mapping = aes(x = time, y = value)) +
+  geom_errorbar(mapping = aes(ymin = value - sd, ymax = value + sd,
+                              colour = speed),
+                width = 0.05) +
+  geom_point(mapping = aes(colour = speed),
+             shape = 19,
+             size = 3) +
+  geom_line(mapping = aes(group = speed,
+                          colour = speed)) +
+  labs(
+    x = "",
+    y = "Asymmetry Index",
+    colour = "Speed"
+  ) +
+  guides(group = "none") +
+  scale_x_discrete(labels = plot_lab,
+                   expand = c(0.15, 0)) +
+  scale_y_continuous(expand = c(0,0),
+                     limits = c(-1,1)) +
+  scale_color_manual(labels = c("2.5 m/s", "3.0 m/s", "3.5 m/s"),
+                     values = c("#FC8D59", "#D7301F", "#7F0000")) +
+  theme_classic() +
+  theme(legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.text = element_text(face = "bold", size = text_size),
+        legend.title = element_text(face = "bold", size = text_size),
+        axis.text.x = element_text(colour = "black", face = "bold", 
+                                   size = text_size, angle = text_angle),
+        axis.title.y = element_text(face = "bold", size = text_size),
+        strip.placement = "outside",
+        strip.text = element_text(face = "bold", size = text_size + 2),
+        strip.background = element_blank()) +
+  facet_wrap(vars(variable),
+             labeller = asym_facet_lab)
+
+print(peak_plot)
+print(cv_plot)
+print(asym_plot)
+
+ggsave("peak_plot.png", plot = peak_plot)
+ggsave("cv_plot.png", plot = cv_plot)
+ggsave("asym_plot.png", plot = asym_plot)
 
