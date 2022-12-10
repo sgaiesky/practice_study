e977c1f51fd96cb939385d5005ab88ee86f0e55a
sgaiesky <sgaiesky@gmail.com>
2022-12-09 21:58:06 -0800

    addition of excel export

--- srs_stats.R
+++ srs_stats.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 
 source('basic_lib.R')
+library("psych")
+library("xlsx")
 
 pivot_dat <- read_xlsx('run_data.xlsx') %>%
   filter(!grepl('remove', note)) %>%
@@ -19,5 +21,57 @@ ggplot(data = filter(pivot_dat, id == x),
   
 }
 
-lapply(unique(pivot_dat$id), prelim.plot)
+#lapply(unique(pivot_dat$id), prelim.plot)
+
+#descriptives
+
+descrip <- pivot_dat %>%
+  group_by(session, speed, variables) %>%
+  summarise(avg = mean(value, na.rm = TRUE),
+            sd = sd(value, na.rm = TRUE))
+
+sd_tbl <- descrip %>%
+  select(!avg) %>%
+  pivot_wider(names_from = session, values_from = sd) %>%
+  rename('sd_1' = '1',
+         'sd_2' = '2') %>%
+  rowwise() %>%
+  mutate(sd_mean = mean(sd_1:sd_2))
+
+icc_res <- list()
+
+for (spd in unique(pivot_dat$speed)) {
+  nam <- paste0('icc_', spd)
+  
+  icc_res[[paste0(nam)]] <- list()
+
+  for (var in unique(pivot_dat$variables)) {
+    dat <- pivot_dat %>%
+      filter(speed == spd & grepl(paste0(var), variables)) %>%
+      pivot_wider(names_from = session, values_from = value) %>%
+      na.omit()
+    
+    x <- ICC(dat[4:5], missing = TRUE)
+    
+    icc_res[[nam]][[paste0(var)]] <- x$results
+    
+    rm(x, dat)
+  }
+}
+
+file_name <- "icc_results.xlsx"
+
+if (file.exists(file_name)) {
+  unlink(file_name)
+  print("File is deleted..")
+} else{
+  print("File not exists..")
+}
+
+for (i in 1:length(icc_res)) {
+  
+write.xlsx(icc_res[[i]], paste0('icc_results.xlsx'), sheetName = names(icc_res[i]), append = TRUE)
+  
+}
+
 
